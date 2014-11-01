971119d3c60ce0082d7da7a7f5a48b3c3b2bbe37
agj <ale@agj.cl>
2014-10-31 22:51:31 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Userscripts
+
+For use with the Greasemonkey web browser extension and similar means, to modify the appearance and operation of certain websites.
+
---
+++ imdb-reprioritize.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        IMDB Re-prioritize
+// @namespace   http://www.agj.cl/
+// @include     /^http:\/\/(www.)?imdb.com\/title\/.+$/
+// @version     0.0.1
+// @grant       none
+// ==/UserScript==
+
+
+// Utils.
+
+function onLoad(cb) {
+	if (/interactive|complete/.test(document.readyState)) setTimeout(cb, 0);
+	else document.addEventListener('DOMContentLoaded', cb);
+}
+var sel = document.querySelector.bind(document);
+
+
+// Modify stuff.
+
+onLoad( function () {
+
+	if (window.top !== window.self) return; // Prevent loading on iframes.
+
+	var centerBottom = sel('#maindetails_center_bottom');
+	var details = sel('#titleDetails');
+
+	var after = sel('#titleMediaStrip');
+	if (!after) after = sel('#titleRecs');
+
+	centerBottom.insertBefore(details, after);
+
+});
+
