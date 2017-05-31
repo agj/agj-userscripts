5e1bdece85c183a83d182d860156435ca736ea41
agj <ale@agj.cl>
2017-05-31 17:17:37 -0400

    cdjournal-to-musicbrainz v0.1.1: Fixed not getting correct data when no '原題' field is present.

--- cdjournal-to-musicbrainz.user.js
+++ cdjournal-to-musicbrainz.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://www.agj.cl/
 // @description Adds link to CDJournal entry page that prefills the MusicBrainz "add release" form.
 // @include     http*://artist.cdjournal.com/d/*
-// @version     0.1.0
+// @version     0.1.1
 // @grant       none
 // ==/UserScript==
 
@@ -15,6 +15,7 @@
 
 const sel = document.querySelector.bind(document);
 const selAll = document.querySelectorAll.bind(document);
+const selIn = (el, selector) => el.querySelector(selector);
 const get = prop => obj => obj[prop];
 const esc = encodeURIComponent;
 const dom = (tag, attrs, ...children) => {
@@ -30,14 +31,21 @@ const flatten = list => list.reduce((r, item) => Array.isArray(item) ? r.concat(
 
 // Get values.
 
-const artist = sel('#discdata_right_body .discdata li:nth-of-type(1) div').textContent;
-const title  = sel('#discdata_right_body .discdata li:nth-of-type(2) div').textContent;
-const type   = sel('#discdata_right_body .discdata li:nth-of-type(3) div').textContent.split(' /')[0];
-const label  = sel('#discdata_right_body .discdata li:nth-of-type(4) div').textContent;
-const cat    = sel('#discdata_right_body .discdata li:nth-of-type(5) div').textContent;
-const date   = sel('#discdata_right_body .discdata li:nth-of-type(7) div').textContent.split('/');
-const tracks = Array.from(selAll('.songlist .song .song_title'))
-               .map(get('textContent'));
+const values =
+    Array.from(selAll('#discdata_right_body .discdata li'))
+    .reduce((r, el) => {
+        const text = el.textContent;
+        if      (/^アーティスト/.test(text)) r.artist = selIn(el, 'div').textContent;
+        else if (/^原題/.test(text))       r.title = selIn(el, 'div').textContent;
+        else if (/^形態/.test(text))       r.type = selIn(el, 'div').textContent.split(' /')[0];
+        else if (/^レーベル/.test(text))    r.label = selIn(el, 'div').textContent;
+        else if (/^規格品番/.test(text))    r.cat = selIn(el, 'div').textContent;
+        else if (/^発売日/.test(text))      r.date = selIn(el, 'div').textContent.split('/');
+        return r;
+    }, {});
+values.tracks = Array.from(selAll('.songlist .song .song_title'))
+                .map(get('textContent'));
+if (!('title' in values)) values.title = sel('#center_body h1').textContent.match(/ \/ (.+)$/)[1];
 
 
 // Add submit link.
@@ -50,14 +58,14 @@ const checkType = raw =>   raw === 'アルバム' ? 'album'
 const link = dom('a', null, 'MusicBrainz に投稿');
 const input = (name, value) => dom('input', { name: name, value: value, type: 'text' });
 const form = dom('form', { name: 'musicbrainz-submit', action: 'https://musicbrainz.org/release/add', method: 'post', 'accept-charset': 'utf-8', style: 'display: none' },
-             	input('name', title),
-             	input('artist_credit.names.0.name', artist),
-             	input('type', checkType(type)),
-             	input('labels.0.name', label),
-             	input('labels.0.catalog_number', cat),
-             	input('events.0.date.year', date[0]),
-             	input('events.0.date.month', date[1]),
-             	input('events.0.date.day', date[2]),
+             	input('name', values.title),
+             	input('artist_credit.names.0.name', values.artist),
+             	input('type', checkType(values.type)),
+             	input('labels.0.name', values.label),
+             	input('labels.0.catalog_number', values.cat),
+             	input('events.0.date.year', values.date[0]),
+             	input('events.0.date.month', values.date[1]),
+             	input('events.0.date.day', values.date[2]),
              	input('events.0.country', 'JP'),
              	input('language', 'jpn'),
              	input('script', 'Jpan'),
@@ -68,7 +76,7 @@ const form = dom('form', { name: 'musicbrainz-submit', action: 'https://musicbra
 const container = dom('div', { id: 'musicbrainz-submit' }, link, form);
 
 const trackCount = counter();
-flatten(tracks
+flatten(values.tracks
         .map(title => {
         	const i = trackCount();
         	return [input(`mediums.0.track.${ i }.name`, title),
