7f6fc626f5c01e580fe34b46d73d8b921c173175
agj <agj_chile@hotmail.com>
2021-06-21 14:59:57 -0400

    pocket-direct-links: v5.0.0
    Since the website was changed again, the code had to be changed to accomodate it. Luckily no weird hacks are needed now, and things work much better!

--- Pocket direct links/pocket-direct-links.user.js
+++ Pocket direct links/pocket-direct-links.user.js
@@ -1,97 +1,67 @@
 // ==UserScript==
 // @name        Pocket direct links
-// @version     4.0.0
+// @version     5.0.0
 // @namespace   http://www.agj.cl/
 // @description Clicking on an item directly opens the website, not the Pocket reader.
 // @license     Unlicense
-// @include     http*://getpocket.com/*
+// @match       *://getpocket.com/*
 // @grant       none
 // ==/UserScript==
 
-const onLoad = cb => /interactive|complete/.test(document.readyState) ? setTimeout(cb, 0) : document.addEventListener('DOMContentLoaded', cb);
+const onLoad = (cb) =>
+  /interactive|complete/.test(document.readyState)
+    ? setTimeout(cb, 0)
+    : document.addEventListener("DOMContentLoaded", cb);
 const sel = document.querySelector.bind(document);
 const selAll = document.querySelectorAll.bind(document);
-const tap = f => (...args) => { f(...args); return args[0] };
-const log = tap(console.log);
 const onChanged = (el, cb) => {
-	if (!el) throw `onChanged: No element passed.`;
-	if (!cb) throw `onChanged: No callback passed.`;
-	const observer = new MutationObserver(cb);
-	observer.observe(el, { childList: true, subtree: true });
-	return observer.disconnect.bind(observer);
+  if (!el) throw `onChanged: No element passed.`;
+  if (!cb) throw `onChanged: No callback passed.`;
+  const observer = new MutationObserver(cb);
+  observer.observe(el, { childList: true, subtree: true });
+  return observer.disconnect.bind(observer);
 };
 
-const attrFixedFlag = 'data-link-fixed-agj';
-const getReactKey = (el) =>
-	Object.keys(el).find((key) => /^__reactProps/.test(key));
-const getUrl = (el) => // Huge hack to get the URL from deep within the React component!
-	el[getReactKey(el)]
-	.children[3]
-	.props
-	.children[1]
-	.props
-	.children
-	.props
-	.children[1]
-	.props
-	.openUrl;
-
+const attrFixedFlag = "data-link-fixed-agj";
+const getUrl = (el) => el.querySelector("a.publisher").getAttribute("href");
 
 onLoad(() => {
-	// Actual link fixing.
+  // Actual link fixing.
 
-	const fix = () => {
-		Array.from(unsafeWindow.document.querySelectorAll('article'))
-			.forEach(fixEl);
-	};
-	const fixEl = (el) => {
-		const url = getUrl(el);
+  const fix = () => {
+    Array.from(document.querySelectorAll("article")).forEach(fixOne);
+  };
 
-		const id = el.getAttribute('data-cy');
-		const linkEl = el.querySelector('a');
-		const safeEl = sel(`[data-cy="${ id }"]`);
-		const safeLinkEl = safeEl.querySelector('a');
+	const fixOne = (el) => {
+    const url = getUrl(el);
+    if (!el.getAttribute(attrFixedFlag)) {
+      const links = el.querySelectorAll(".content .title a, .cardWrap a");
+      links.forEach((linkEl) => linkEl.setAttribute("href", url));
+      el.setAttribute(attrFixedFlag, true);
+    }
+  };
 
-		if (linkEl.getAttribute(attrFixedFlag)) {
-			return;
-		}
+  // Fix when links added.
 
-		linkEl.setAttribute('href', url);
+  onChanged(sel("#__next"), fix);
+  fix();
 
-		safeLinkEl.addEventListener('click', (e) => {
-			if (!e.getModifierState('Shift') && !e.getModifierState('Alt') && !e.getModifierState('Meta') && !e.getModifierState('Control')) {
-				e.stopPropagation();
-				e.preventDefault();
-				window.location.href = url;
-			}
-		});
+  // Fix when history state changed.
 
-		linkEl.setAttribute(attrFixedFlag, true);
-	};
+  const pushState = history.pushState.bind(history);
+  const replaceState = history.replaceState.bind(history);
+  const locationChanged = () => {
+    fix();
+  };
 
-	// Fix when links added.
+  history.pushState = (...args) => {
+    pushState(...args);
+    locationChanged();
+  };
+  history.replaceState = (...args) => {
+    replaceState(...args);
+    locationChanged();
+  };
 
-	onChanged(sel('#__next'), fix);
-	fix();
-
-	// Fix when history state changed.
-
-	const pushState = history.pushState.bind(history);
-	const replaceState = history.replaceState.bind(history);
-	const locationChanged = () => {
-		fix();
-	};
-
-	history.pushState = (...args) => {
-		pushState(...args);
-		locationChanged();
-	};
-	history.replaceState = (...args) => {
-		replaceState(...args);
-		locationChanged();
-	};
-
-	window.addEventListener('popstate', locationChanged);
+  window.addEventListener("popstate", locationChanged);
 });
-
-
