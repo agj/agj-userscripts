e2bcb733954cab07cb68309eedc44492ada695a0
agj <agj_chile@hotmail.com>
2022-02-27 16:47:56 +0800

    Start reworking Taiwan ISRC for new design

--- Taiwan ISRC to MusicBrainz/taiwan-isrc-to-musicbrainz.user.js
+++ Taiwan ISRC to MusicBrainz/taiwan-isrc-to-musicbrainz.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name            Taiwan ISRC to MusicBrainz
-// @version         1.1.0
+// @version         2.0.0
 // @namespace       http://www.agj.cl/
 // @description     Adds a link to any isrc.ncl.edu.tw record entry page that opens the Add Release form in MusicBrainz, prefilling it with that record's information.
 // @license         Unlicense
-// @include         http*://isrc.ncl.edu.tw/music_view.aspx*
+// @include         http*://isrc.ncl.edu.tw/C100/*
 // @grant           none
 // ==/UserScript==
 
@@ -43,87 +43,101 @@
         Array.isArray(item) ? r.concat(flatten(item)) : r.concat([item]),
       []
     );
+  const onFullLoad = (cb) =>
+    /complete/.test(document.readyState)
+      ? setTimeout(cb, 0)
+      : window.addEventListener("load", cb, {
+          once: true,
+        });
+  const waitFor = (milliseconds) =>
+    new Promise((resolve) => {
+      setTimeout(resolve, milliseconds);
+    });
 
-  // Get values.
+  onFullLoad(async () => {
+    // Get values.
 
-  const values = Array.from(selAll(".musicView > table > tbody > tr")).reduce(
-    (r, el) => {
-      const label = selIn(el, "th").textContent.trim();
-      const value = selIn(el, "td")
-        .textContent.trim()
-        .replace(/^:\s+(\S.*)$/, "$1");
-      if (/表演者/.test(label)) r.artist = value;
-      else if (/樂團名稱/.test(label)) r.artist = value;
-      else if (/專輯名稱/.test(label)) r.title = value;
-      else if (/發行公司/.test(label)) r.label = value;
-      else if (/產品編碼/.test(label)) r.cat = value;
-      else if (/EAN碼/.test(label)) r.barcode = value;
-      else if (/發行日期/.test(label)) r.date = value.split("/");
-      return r;
-    },
-    {}
-  );
-  values.tracks = Array.from(selAll(".AllMusic fieldset a"))
-    .map(get("textContent"))
-    .map(call("trim"))
-    .map((t) => t.replace(/^.+ ] 曲目名稱:(.*)$/, "$1"));
+    await waitFor(1000);
 
-  // Add submit link.
+    const values = Array.from(sel(".table").querySelectorAll("tr")).reduce(
+      (r, el) => {
+        const label = selIn(el, "th").textContent.trim();
+        const value = selIn(el, "td").textContent.trim();
+        if (/表演者/.test(label)) r.artist = value;
+        else if (/樂團名稱/.test(label)) r.artist = value;
+        else if (/專輯名稱/.test(label)) r.title = value;
+        else if (/發行公司/.test(label)) r.label = value;
+        else if (/產品編碼/.test(label)) r.cat = value;
+        else if (/EAN\/UPC碼/.test(label)) r.barcode = value;
+        else if (/發行日期/.test(label)) r.date = value.split(".");
+        return r;
+      },
+      {}
+    );
 
-  const checkValue = (value) => (value === undefined ? "" : value);
-  const input = (name, value) =>
-    dom("input", { name: name, value: checkValue(value), type: "text" });
+    values.tracks = Array.from(selAll("#songsTable a"))
+      .map(get("textContent"))
+      .map(call("trim"))
+      .map((t) => t.replace(/^\[\S+\] \d+[.](.*)$/, "$1"));
 
-  const link = dom("a", null, "Add to MusicBrainz");
-  const form = dom(
-    "form",
-    {
-      name: "musicbrainz-submit",
-      action: "https://musicbrainz.org/release/add",
-      method: "post",
-      "accept-charset": "utf-8",
-      style: "display: none",
-    },
-    input("name", values.title),
-    input("artist_credit.names.0.name", values.artist),
-    input("labels.0.name", values.label),
-    input("labels.0.catalog_number", values.cat),
-    input("events.0.date.year", values.date[0]),
-    input("events.0.date.month", values.date[1]),
-    input("events.0.country", "TW"),
-    input("barcode", values.barcode),
-    input("urls.0.url", window.location.href),
-    input("urls.0.link_type", "82"),
-    input("language", "cmn"),
-    input("script", "Hant"),
-    input("status", "official"),
-    input("mediums.0.format", "cd"),
-    input("edit_note", "From Taiwan ISRC: " + window.location.href)
-  );
-  const container = dom("div", { id: "musicbrainz-submit" }, link, form);
+    console.log({ values });
 
-  const trackCount = counter();
-  flatten(
-    values.tracks.map((title) => {
-      const i = trackCount();
-      return [
-        input(`mediums.0.track.${i}.name`, title),
-        input(`mediums.0.track.${i}.number`, i + 1),
-      ];
-    })
-  ).map((el) => form.appendChild(el));
+    // Add submit link.
 
-  sel("#mainContent > div").prepend(container);
-  link.addEventListener("click", (e) => {
-    form.submit();
-    e.preventDefault();
-  });
+    const checkValue = (value) => (value === undefined ? "" : value);
+    const input = (name, value) =>
+      dom("input", { name: name, value: checkValue(value), type: "text" });
 
-  sel("head").append(
-    dom(
-      "style",
-      null,
-      `
+    const link = dom("a", null, "Add to MusicBrainz");
+    const form = dom(
+      "form",
+      {
+        name: "musicbrainz-submit",
+        action: "https://musicbrainz.org/release/add",
+        method: "post",
+        "accept-charset": "utf-8",
+        style: "display: none",
+      },
+      input("name", values.title),
+      input("artist_credit.names.0.name", values.artist),
+      input("labels.0.name", values.label),
+      input("labels.0.catalog_number", values.cat),
+      input("events.0.date.year", values.date[0]),
+      input("events.0.date.month", values.date[1]),
+      input("events.0.country", "TW"),
+      input("barcode", values.barcode),
+      input("urls.0.url", window.location.href),
+      input("urls.0.link_type", "82"),
+      input("language", "cmn"),
+      input("script", "Hant"),
+      input("status", "official"),
+      input("mediums.0.format", "cd"),
+      input("edit_note", "From Taiwan ISRC: " + window.location.href)
+    );
+    const container = dom("div", { id: "musicbrainz-submit" }, link, form);
+
+    const trackCount = counter();
+    flatten(
+      values.tracks.map((title) => {
+        const i = trackCount();
+        return [
+          input(`mediums.0.track.${i}.name`, title),
+          input(`mediums.0.track.${i}.number`, i + 1),
+        ];
+      })
+    ).map((el) => form.appendChild(el));
+
+    sel("#mainContent > div").prepend(container);
+    link.addEventListener("click", (e) => {
+      form.submit();
+      e.preventDefault();
+    });
+
+    sel("head").append(
+      dom(
+        "style",
+        null,
+        `
         #musicbrainz-submit {
             display: inline-flex;
             vertical-align: bottom;
@@ -137,6 +151,7 @@
             font-weight: bold;
         }
 `
-    )
-  );
+      )
+    );
+  });
 })();
